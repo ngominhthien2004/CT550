8c36d08b5dde19179b0a2400849333293d7b373e
ngominhthien2004 <ngominhthien2004@gmail.com>
2026-06-29 19:30:44 +0700

    fix: change report icon from SVG layers to Font Awesome flag

--- frontend/src/components/artwork/ArtworkCard.vue
+++ frontend/src/components/artwork/ArtworkCard.vue
@@ -94,7 +94,7 @@ function getImageCount(item) {
       </button>
       <!-- Report button -->
       <button type="button" v-if="isLoggedIn" class="btn-report" @click.stop="showReportModal = true" title="Report artwork">
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/></svg>
+        <i class="fa-regular fa-flag" aria-hidden="true"></i>
       </button>
     </div>
 
