d3ffbff8997f1cf9c8233dd575c38732a3bbef5e
ngominhthien2004 <ngominhthien2004@gmail.com>
2026-07-04 15:25:39 +0700

    fix: banner kicker text showing correct tab name
    Pass pageTitle as kickerText to HomeHeroBanner in TypedHomeFeedView
    so Illustrations/Manga/GIF tabs show correct label instead of 'Home'

--- frontend/src/views/TypedHomeFeedView.vue
+++ frontend/src/views/TypedHomeFeedView.vue
@@ -210,7 +210,7 @@ watch(
         </div>
 
         <HomeTagStrip :tags="liveTags" />
-        <HomeHeroBanner :slide="heroSlide" :banner-link="bannerLink" />
+        <HomeHeroBanner :slide="heroSlide" :banner-link="bannerLink" :kicker-text="pageTitle" />
         <p v-if="isLoading" class="type-loading">Loading {{ pageTitle.toLowerCase() }}...</p>
         <HomeArtworkGrid :works="spotlightWorks" />
 
